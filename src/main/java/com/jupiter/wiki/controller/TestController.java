92e17adbe029019876e23a16b6d1e4de120543ca
louyecong <luoyecong@sdbattery.com>
2021-12-12 10:50:08 +0800

    feat: http client test

--- src/main/java/com/jupiter/wiki/controller/TestController.java
+++ src/main/java/com/jupiter/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.jupiter.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,9 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World!" + name;
     }
 }
